0b18089253b5a3deb6da44a2b0944cb42de93be4
Echo <longshensanjie@gmail.com>
2025-01-20 15:49:11 +0800

    Update user.ts
    retore change

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -1,55 +1,7 @@
-import type { Config } from "~/types";
+import type { UserConfig } from '~/types'
 
-export const configUser: Partial<Config> = {
-	title: "泥封",
-	author: "Echo",
-	desc: "Rediscory the beauty of typography",
-	website: "https://astro-theme-typography.vercel.app/",
-	locale: "en-us",
-	themeStyle: "light",
-	socials: [
-		{
-			name: "github",
-			href: "https://github.com/moeyua/astro-theme-typography",
-		},
-		{
-			name: "rss",
-			href: "/atom.xml",
-		},
-		{
-			name: "twitter",
-			href: "https://github.com/moeyua/astro-theme-typography",
-		},
-		{
-			name: "mastodon",
-			href: "https://github.com/moeyua/astro-theme-typography",
-		},
-	],
-	header: {
-		twitter: "@moeyua13",
-	},
-	navs: [
-		{
-			name: "Posts",
-			href: "/posts/page/1",
-		},
-		{
-			name: "Archive",
-			href: "/archive",
-		},
-		{
-			name: "Categories",
-			href: "/categories",
-		},
-		{
-			name: "About",
-			href: "/about",
-		},
-	],
-	category_map: [{ name: "胡适", path: "hu-shi" }],
-	comments: {
-		disqus: {
-			shortname: "typography-astro",
-		},
-	},
-};
+export const userConfig: Partial<UserConfig> = {
+  // Override the default config here
+  // site: { title: "講評世界" },
+  // seo: { twitter: "@moeyua13" },
+}
